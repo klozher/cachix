3f2a128ba302c4def2f1a17572529b0765a1e7a5
indtability <indtability@outlook.com>
2025-09-11 02:33:17 +0800

    wine: remove flag --with-mingw=clang

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
         packages.aarch64-linux.wine = with nixpkgs.legacyPackages.aarch64-linux; wineWow64Packages.minimal.overrideAttrs
             (prevAttrs: {
                 meta.platforms = prevAttrs.meta.platforms ++ [ "aarch64-linux" ];
-                configureFlags = (builtins.filter (flag: flag != "--enable-archs=x86_64,i386") prevAttrs.configureFlags) ++ [ "--enable-archs=aarch64,i386" "--with-mingw=clang" ];
+                configureFlags = (builtins.filter (flag: flag != "--enable-archs=x86_64,i386") prevAttrs.configureFlags) ++ [ "--enable-archs=aarch64,i386" ];
                 nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgsCross.ucrtAarch64.buildPackages.clang_21 ];
             });
     };
