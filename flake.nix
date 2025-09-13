d5f5be05144df1705d181ab4eb8b41d9dd744329
indtability <indtability@outlook.com>
2025-09-13 20:56:04 +0800

    wine: try nixos default clang version

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         wine_overrideAttrs = with nixpkgs.legacyPackages.aarch64-linux; prevAttrs: {
             meta.platforms = prevAttrs.meta.platforms ++ [ "aarch64-linux" ];
             configureFlags = (builtins.filter (flag: flag != "--enable-archs=x86_64,i386") prevAttrs.configureFlags) ++ [ "--enable-archs=aarch64,i386" ];
-            nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgsCross.ucrtAarch64.buildPackages.clang_21 ];
+            nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgsCross.ucrtAarch64.buildPackages.clang ];
         };
         wine = nixpkgs.legacyPackages.aarch64-linux.wineWow64Packages.minimal.overrideAttrs wine_overrideAttrs;
     in {
