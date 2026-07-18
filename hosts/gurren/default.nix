b84c68e268bae2d23920898e5e70a12e6a36ce4f
klozher <klozher@outlook.com>
2026-07-18 22:32:09 +0800

    gurren: wivrn and cuda

--- hosts/gurren/default.nix
+++ hosts/gurren/default.nix
@@ -8,6 +8,7 @@
     klozher.home-manager.enable = true;
     klozher.home-manager.users.sice = import ./home.nix;
 
+    nixpkgs.config.cudaSupport = true;
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
     boot.kernelModules = ["ntsync"];
     virtualisation.libvirtd.enable = true;
@@ -30,27 +31,12 @@
     services.scx.enable = true;
     services.scx.scheduler = "scx_lavd";
 
-    #    services.wivrn = {
-    #        enable = true;
-    #        openFirewall = true;
-    #
-    #        # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
-    #        # will automatically read this and work with WiVRn (Note: This does not currently
-    #        # apply for games run in Valve's Proton)
-    #          defaultRuntime = true;
-    #
-    #        # Run WiVRn as a systemd service on startup
-    #        autoStart = true;
-    #
-    #        steam.importOXRRuntimes = true;
-    #        highPriority = true;
-    #
-    #        # If you're running this with an nVidia GPU and want to use GPU Encoding (and don't otherwise have CUDA enabled system wide), you need to override the cudaSupport variable.
-    #        #package = (pkgs.wivrn.override { cudaSupport = true; });
-    #
-    #        # You should use the default configuration (which is no configuration), as that works the best out of the box.
-    #        # However, if you need to configure something see https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md for configuration options and https://mynixos.com/nixpkgs/option/services.wivrn.config.json for an example configuration.
-    #    };
+    services.wivrn = {
+        enable = true;
+        openFirewall = true;
+        steam.importOXRRuntimes = true;
+        highPriority = true;
+    };
     services.samba = {
         enable = true;
         openFirewall = true;
