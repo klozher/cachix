9a555eceb57f989d7064871d6fc1b89c8794bc79
klozher <klozher@outlook.com>
2026-07-27 03:05:57 +0800

    nixvim: enable focus-nvim and grug-far.nvim

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -7,54 +7,64 @@ in {
         enable = lib.mkEnableOption "Enable neovim";
     };
     config = lib.mkIf cfg.enable {
-      programs.nixvim = {
-        enable = true;
-        defaultEditor = true;
-        nixpkgs.useGlobalPackages = true;
-        viAlias = true;
-        vimAlias = true;
-        colorscheme = "tokyonight";
-        colorschemes.tokyonight = {
-          enable = true;
-        };
-        opts = {
-          shiftwidth = 4;
-          softtabstop = -1;
-          expandtab = true;
-          list = true;
-          wrap = false;
-          ignorecase = true;
-          smartcase = true;
-          number = true;
-        };
-        plugins = {
-          lsp = {
+        programs.nixvim = {
             enable = true;
-            servers = {
-              bashls.enable = true;
-              clangd.enable = true;
-              nixd.enable = true;
-              pylsp.enable = true;
+            defaultEditor = true;
+            nixpkgs.useGlobalPackages = true;
+            viAlias = true;
+            vimAlias = true;
+            colorscheme = "tokyonight";
+            colorschemes.tokyonight = {
+                enable = true;
             };
-          };
-          fidget.enable = true;
-          trouble.enable = true;
-          web-devicons.enable = true;
-          treesitter = {
-            enable = true;
-            nixGrammars = true;
-            settings = {
-              highlight.enable = true;
-              indent.enable = true;
+            opts = {
+                shiftwidth = 4;
+                softtabstop = -1;
+                expandtab = true;
+                list = true;
+                wrap = false;
+                ignorecase = true;
+                smartcase = true;
+                number = true;
             };
-          };
-          treesitter-context = {
-            enable = true;
-            settings = { max_lines = 2; };
-          };
-          rainbow-delimiters.enable = true;
+            plugins = {
+                lsp = {
+                    enable = true;
+                    servers = {
+                        bashls.enable = true;
+                        clangd.enable = true;
+                        nixd.enable = true;
+                        pylsp.enable = true;
+                    };
+                };
+                fidget.enable = true;
+                trouble.enable = true;
+                web-devicons.enable = true;
+                treesitter = {
+                    enable = true;
+                    nixGrammars = true;
+                    settings = {
+                        highlight.enable = true;
+                        indent.enable = true;
+                    };
+                };
+                treesitter-context = {
+                    enable = true;
+                    settings = { max_lines = 2; };
+                };
+                rainbow-delimiters.enable = true;
+                grug-far.enable = true;
+            };
+            extraPlugins = [
+                pkgs.vimPlugins.focus-nvim
+            ];
+            extraConfigLua = ''
+                require("focus").setup()
+            '';
+            extraPackages = [
+                pkgs.ripgrep
+            ];
         };
-      };
     };
 }
 
