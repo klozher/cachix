ef1bdc4b6c70e66d4040c9e91a8eb8cf4b2cf714
klozher <klozher@outlook.com>
2026-07-27 02:53:56 +0800

    state: fix firefox behavior

--- modules/state.nix
+++ modules/state.nix
@@ -3,6 +3,7 @@
     system.stateVersion = "25.11";
     home-manager.sharedModules = [({config, osConfig, ...}: {
         home.stateVersion = osConfig.system.stateVersion;
+        programs.firefox.configPath = "${config.xdg.configHome}/mozilla/firefox";
     })];
 }
 
